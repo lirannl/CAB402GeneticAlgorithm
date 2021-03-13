e855d32e5570d20be050b790e761a612d528b48d
lirannl <liranpiade@gmail.com>
2021-03-14 06:21:35 +1000

    Added random operations on iterables

---
+++ CSharpSchedule/RandomOps.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpGeneticAlgorithm
+{
+    class RandomOps
+    {
+        // Randomly pick an item from a list-like object
+        public static T Pick<T>(IList<T> items, Random rand)
+        {
+            int index = rand.Next(0, items.Count);
+            return items[index];
+        }
+
+        // Randomly rearrange the items in an array
+        public static T[] Shuffle<T>(T[] items, Random rand)
+        {
+            List<T> itemsList = items.ToList();
+
+            int outputIdx = 0;
+            foreach (int i in Enumerable.Range(0, items.Length))
+            {
+                // Take a random item from the list
+                T item = Pick(itemsList, rand);
+                itemsList.Remove(item);
+                // Write the item to the array
+                items[outputIdx] = item;
+                // Increment the position
+                outputIdx++;
+            }
+            return items;
+        }
+    }
+}
