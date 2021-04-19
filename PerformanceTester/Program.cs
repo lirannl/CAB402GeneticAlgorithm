37e2b6cc8b4e6c15babb16e1688b33fffd26a813
lirannl <liranpiade@gmail.com>
2021-04-19 13:11:38 +1000

    Added a runtime timespan measurement

--- PerformanceTester/Program.cs
+++ PerformanceTester/Program.cs
@@ -13,9 +13,11 @@ namespace PerformanceTester
             var steps = 1000;
             var cities = RandomMonad.evaluateWith(new Random(), TSP.generateRandomCities(indivsNum));
             FSharpFunc<int[], double> fitnessFunc = TSP.TSPCost(cities);
-            Func<FSharpFunc<int[], double>, int, int, IEnumerable<Tuple<int[], double>>> optimise = CAB402.CSharp.GeneticAlgorithm.Optimize;
+            Func<FSharpFunc<int[], double>, int, int, IEnumerable<Tuple<int[], double>>> optimise = CAB402.FSharp.GeneticAlgorithm.Optimize;
+            var startTime = DateTime.Now;
             var results = optimise(fitnessFunc, indivsNum, indivsNum).Take(steps).ToArray();
-            Console.WriteLine("Hello World!");
+            var duration = DateTime.Now - startTime;
+            Console.WriteLine(string.Format("Evovled {0} generations in {1} seconds!", results.Length, duration.TotalSeconds));
         }
     }
 }
