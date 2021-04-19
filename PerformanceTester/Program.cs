51d2af217c72140a6537a15502353bd6ae067ca0
lirannl <liranpiade@gmail.com>
2021-04-19 13:35:07 +1000

    Modified the performance tester to dynamically select an algorithm

--- PerformanceTester/Program.cs
+++ PerformanceTester/Program.cs
@@ -7,17 +7,27 @@ namespace PerformanceTester
 {
     class Program
     {
+        static Func<FSharpFunc<int[], double>, int, int, IEnumerable<Tuple<int[], double>>> Optimize(String lang)
+        {
+            switch (lang) 
+            {
+                case "fsharp" : return CAB402.FSharp.GeneticAlgorithm.Optimize;
+                case "csharp": return CAB402.CSharp.GeneticAlgorithm.Optimize;
+                default: throw new ArgumentOutOfRangeException("No such option.");
+            }
+        }
+
         static void Main()
         {
             var indivsNum = 100;
             var steps = 1000;
             var cities = RandomMonad.evaluateWith(new Random(), TSP.generateRandomCities(indivsNum));
             FSharpFunc<int[], double> fitnessFunc = TSP.TSPCost(cities);
-            Func<FSharpFunc<int[], double>, int, int, IEnumerable<Tuple<int[], double>>> optimise = CAB402.FSharp.GeneticAlgorithm.Optimize;
+            var lang = "csharp";
             var startTime = DateTime.Now;
-            var results = optimise(fitnessFunc, indivsNum, indivsNum).Take(steps).ToArray();
+            var results = Optimize(lang)(fitnessFunc, indivsNum, indivsNum).Take(steps).ToArray();
             var duration = DateTime.Now - startTime;
-            Console.WriteLine(string.Format("Evovled {0} generations in {1} seconds!", results.Length, duration.TotalSeconds));
+            Console.WriteLine(string.Format("Evovled {0} generations in {1} seconds, using {2}!", results.Length, duration.TotalSeconds, lang));
         }
     }
 }
