81dd3ebdcb8cf8431719452bc63bc706b20f0ae3
lirannl <liranpiade@gmail.com>
2021-04-19 12:26:45 +1000

    Added a performance tester

---
+++ PerformanceTester/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.FSharp.Core;
+
+namespace PerformanceTester
+{
+    class Program
+    {
+        static void Main()
+        {
+            var indivsNum = 100;
+            var steps = 1000;
+            var cities = RandomMonad.evaluateWith(new Random(), TSP.generateRandomCities(indivsNum));
+            FSharpFunc<int[], double> fitnessFunc = TSP.TSPCost(cities);
+            Func<FSharpFunc<int[], double>, int, int, IEnumerable<Tuple<int[], double>>> optimise = CAB402.CSharp.GeneticAlgorithm.Optimize;
+            var results = optimise(fitnessFunc, indivsNum, indivsNum).Take(steps).ToArray();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
